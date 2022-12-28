629edc29dbae275fcc85ec2720a2b28746317664
Adelya Fattakhova <AdIFattakhova@stud.kpfu.ru>
2022-12-28 21:32:39 +0300

    new

--- ControlWork2_1/server.py
+++ ControlWork2_1/server.py
@@ -3,6 +3,7 @@ import threading
 
 connections = []
 total_connections = 0
+tasks = []
 
 
 class Client(threading.Thread):
@@ -27,16 +28,47 @@ class Client(threading.Thread):
                 connections.remove(self)
                 break
             if data != "":
-                print("Пользователь " + str(self.id) + ": " + str(data.decode("utf-8")))
                 data = str(data).replace("b'", " ")
                 data = data.replace("'", " ")
                 data = data[1:]
                 if '/new' in data:
-                    pass
+                    tasks.append(data[4:])
+                    for client in connections:
+                        if client.id != self.id:
+                            client.socket.sendall(str.encode('Запись добавлена'))
+                    print('Запись добавлена')
                 elif '/edit' in data:
-                    pass
+                    if len(tasks) != 0:
+                        try:
+                            spl = data[6:]
+                            print(spl)
+                            task_id = int(data[6:spl.find(' ') + 6])
+                            new_task = data[spl.find(' ') + 7:]
+                            print('new', new_task)
+                            tasks.remove(tasks[task_id])
+                            tasks.insert(task_id, new_task)
+                            print('Запись изменена')
+                        except:
+                            print('Ошибка в команде')
+                    else:
+                        print('Записи отсутствуют')
                 elif '/delete' in data:
-                    pass
+                    if len(tasks) != 0:
+                        try:
+                            task_id = int(data[7:])
+                            tasks.remove(tasks[task_id])
+                            print('Запись удалена')
+                        except:
+                            print('Ошибка в команде')
+                    else:
+                        print('Записи отсутствуют')
+                elif '/show' in data:
+                    if len(tasks) != 0:
+                        print('Текущие записи:')
+                        for i in range(len(tasks)):
+                            print(f' {i} - {tasks[i]}')
+                    else:
+                        print('Записи отсутствуют')
                 else:
                     for client in connections:
                         if client.id != self.id:
@@ -54,11 +86,8 @@ def newConnections(socket):
 
 
 def main():
-    host = input("Host: ")
-    port = int(input("Port: "))
-
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind((host, port))
+    sock.bind(('localhost', 8080))
     sock.listen(5)
 
     newConnectionsThread = threading.Thread(target=newConnections, args=(sock,))
